3399bce2992d2b5940df1f30b4c895537f4369ff
Lim Sim <lsim@equalexperts.com>
2015-03-24 09:22:32 +0000

    Added first 3 lessons.

---
+++ lesson02/src/main/java/Calculator.java
@@ -0,0 +1,8 @@
+/**
+ * Created by limsim on 23/03/15.
+ */
+public class Calculator {
+    public static int add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+}
---
+++ lesson03/src/test/java/CalculatorTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+}
